2a237efe9e455fd3b06891aa86008cd4c1be29fb
SHELJIN SG <sheljin2003sgs@gmail.com>
2023-06-19 12:11:38 +0530

    Updated code

---
+++ code/server/routes/auth.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const bcrypte = require('bcryptjs');
+const auth = require("../middleware/auth");
+const jwtToken = require('jsonwebtoken');
+const { check, validationResult } = require("express-validator");
+
+const userModel = require("../models/user");
+
+router.post("/",
+
+    check("ownerEmail", "ownerEmail is required").notEmpty(),
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { owernEmail } = req.body;
+
+        try {
+
+            let user = await userModel.findOne({ ownerEmail });
+            if (!user) {
+                return res.status(404).json({ msg: "Invalid Values" });
+            }
+
+            const isMatch = await bcrypte.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(404).json({ msg: "Invalid Values" });
+            }
+
+            const payload = { user: { id: user.id } };
+            jwtToken.sign(payload, "jwtSecret", { expiresIn: "10 days" }, (err, token) => {
+                if (err) throw err;
+                else
+                    res.json(token);
+            })
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("Server error");
+        }
+    });
+
+
+module.exports = router;
